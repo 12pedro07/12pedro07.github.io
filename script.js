46d7a9bef55be75240f1fc4e08a8bfdc95c38251
uniefelsousa <62039805+uniefelsousa@users.noreply.github.com>
2020-03-18 17:30:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+function muda_1(){
+  document.getElementById("principal1").style.backgroundColor="#3ea1f8";
+  document.getElementById("principal1").style.borderColor="white";
+}
+
+function muda_2(){
+  document.getElementById("principal2").style.backgroundColor="#3ea1f8";
+  document.getElementById("principal2").style.borderColor="white";
+}
+
+function volta_1(){
+  document.getElementById("principal1").style.backgroundColor="black";
+  document.getElementById("principal1").style.borderColor="#3ea1f8";
+}
+
+function volta_2(){
+  document.getElementById("principal2").style.backgroundColor="black";
+  document.getElementById("principal2").style.borderColor="#3ea1f8";
+}
