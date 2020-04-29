a28a1401035aa05a514a6076a1ebc54ca5db52cd
uniefelsousa <62039805+uniefelsousa@users.noreply.github.com>
2020-04-29 19:57:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ function muda_2(){
   document.getElementById("principal2").style.borderColor="white";
 }
 
+
+function muda_3(){
+  document.getElementById("principal3").style.backgroundColor="#3ea1f8";
+  document.getElementById("principal3").style.borderColor="white";
+}
+
+
+function muda_4(){
+  document.getElementById("principal4").style.backgroundColor="#3ea1f8";
+  document.getElementById("principal4").style.borderColor="white";
+}
+
 function volta_1(){
   document.getElementById("principal1").style.backgroundColor="black";
   document.getElementById("principal1").style.borderColor="#3ea1f8";
@@ -22,4 +34,36 @@ function volta_1(){
 function volta_2(){
   document.getElementById("principal2").style.backgroundColor="black";
   document.getElementById("principal2").style.borderColor="#3ea1f8";
+}
+
+function volta_3(){
+  document.getElementById("principal3").style.backgroundColor="black";
+  document.getElementById("principal3").style.borderColor="#3ea1f8";
+}
+
+function volta_4(){
+  document.getElementById("principal4").style.backgroundColor="black";
+  document.getElementById("principal4").style.borderColor="#3ea1f8";
+}
+
+function Buble()
+{
+location.href=" buble_sort.html"
+
+}
+
+function Insertion(){
+  location.href="insertion_sort.html"
+}
+
+function Quick(){
+  location.href="quick_sort.html"
+}
+
+function Merge(){
+  location.href="merge_sort.html"
+}
+
+function Inicio(){
+  location.href="index.html"
 }
